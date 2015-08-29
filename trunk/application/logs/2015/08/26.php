53eb57472c0a907925a1329cb71f3537b4da8e8e
Light23 <Light23@4fce8022-8193-4c76-a95f-8f9f1b92b11e>
2015-08-29 16:30:58 +0000

    Удаление логов из под репозитория

--- trunk/application/logs/2015/08/26.php
+++
@@ -1,6 +0,0 @@
-<?php defined('SYSPATH') OR die('No direct script access.'); ?>
-
-2015-08-26 00:06:41 --- CRITICAL: ErrorException [ 2 ]: unserialize() [0function.unserialize0]: Function spl_autoload_call() hasn't defined the class it was called for ~ DOCROOT\index.php [ 109 ] in :
-2015-08-26 00:06:41 --- DEBUG: #0 [internal function]: Kohana_Core::error_handler(2, 'unserialize() [...', 'Z:\home\test1.r...', 109, Array)
-#1 Z:\home\test1.ru\www\index.php(109): unserialize('a:1:{s:9:"detai...')
-#2 {main} in :
